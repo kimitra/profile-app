74711068bcaef8f9c572192018d54375913f53fe
kimitra <vmitraki@purdue.edu>
2026-02-19 00:02:09 -0500

    Updated to Lab 11

--- src/context/ModeContext.jsx
+++ src/context/ModeContext.jsx
@@ -3,16 +3,17 @@ import { createContext, useState } from "react";
 const ModeContext = createContext();
 
 export const ModeProvider = ({ children }) => {
-    const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
 
-    const toggleMode = () => {
-        setMode((prev) => (prev === "light" ? "dark" : "light"));
-    };
+  const toggleMode = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
-    return (
-        <ModeContext.Provider value={{ mode, toggleMode }}>
-            {children}
-        </ModeContext.Provider>
-    );
+  return (
+    <ModeContext.Provider value={{ mode, toggleMode }}>
+      {children}
+    </ModeContext.Provider>
+  );
 };
+
 export default ModeContext;
